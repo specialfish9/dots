6f81e5cfcc3a8b3cecfe038f01b47e531fdd1c99
Mattia <mgiro2001@gmail.com>
2021-09-18 23:09:25 +0200

    updated

---
+++ update.sh
@@ -0,0 +1,10 @@
+echo "bspwm"
+cp -r ../.config/bspwm/ .
+echo "sxhkd"
+cp -r ../.config/sxhkd/ .
+echo "polybar"
+cp -r ../.config/polybar/ .
+echo "nvim"
+cp -r ../.config/nvim/init.vim ./nvim/
+echo "fonts"
+cp -r $HOME/.fonts .
