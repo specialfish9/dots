5ed3bbb49e8e93d4e074c8d723d66fb681b69257
Mattia <mgiro2001@gmail.com>
2022-04-24 20:56:21 +0200

    polybar on multiple monitors

--- polybar/start.sh
+++ polybar/start.sh
@@ -4,8 +4,18 @@ pkill -x polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar wsbar -c ~/.config/polybar/config.ini &
-polybar monitorbar -c ~/.config/polybar/config.ini &
-polybar timebar -c ~/.config/polybar/config.ini &
-polybar controlbar -c ~/.config/polybar/config.ini &
-polybar controlbar2 -c ~/.config/polybar/config.ini &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar wsbar -c ~/.config/polybar/config.ini &
+    MONITOR=$m polybar monitorbar -c ~/.config/polybar/config.ini &
+    MONITOR=$m polybar timebar -c ~/.config/polybar/config.ini &
+    MONITOR=$m polybar controlbar -c ~/.config/polybar/config.ini &
+    MONITOR=$m polybar controlbar2 -c ~/.config/polybar/config.ini &
+  done
+else
+    polybar wsbar -c ~/.config/polybar/config.ini &
+    polybar monitorbar -c ~/.config/polybar/config.ini &
+    polybar timebar -c ~/.config/polybar/config.ini &
+    polybar controlbar -c ~/.config/polybar/config.ini &
+    polybar controlbar2 -c ~/.config/polybar/config.ini &
+fi
